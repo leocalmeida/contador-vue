24bd5dab1f7cf0e1a345bc97a73ff91c6c1008fe
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-09-24 17:37:08 -0300

    Contator em VUE

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<template>
+  <Contador />
+</template>
+
+
+<style>
+@import './styles/App.css';
+</style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,59 @@
+<template>
+  <div className="container">
+    <div className="numero">
+      <h1>{{ valor }}</h1>
+    </div>
+
+    <div className="botoes">
+      <input type="button" value="+" v-on:click="handleIncrease" />
+      <input
+        className="input-ml"
+        type="button"
+        value="-"
+        v-on:click="handleDecrease"
+      />
+      <input
+        className="input-ml"
+        type="button"
+        value="Reset"
+        v-on:click="handleReset"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contador",
+  methods: {
+    handleIncrease: function () {
+      if (this.valor >= 99) {
+        this.valor = 0;
+      } else {
+        this.valor++;
+      }
+      return this.valor;
+    },
+    handleDecrease: function () {
+      if (this.valor <= 0) {
+        this.valor = 0;
+      } else {
+        this.valor--;
+      }
+      return this.valor;
+    },
+    handleReset: function () {
+      return (this.valor = 0);
+    },
+  },
+  data: function () {
+    return {
+      valor: 0,
+    };
+  },
+};
+</script>
+
+<style  scoped>
+@import "../styles/Contador.css";
+</style>
